359c1b7c829eed9fd53dd094b1b0deb4dbaed546
Chris Lawrence <cnlawrence1183@gmail.com>
2015-01-24 21:18:17 -0600

    Pushing up code for Peer Review

---
+++ Deck36.UnitSpecs/Web/Helpers/BootstrapHelperSpecs.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using Deck36.Web.Helpers;
+using NUnit.Framework;
+using SpecsFor;
+using SpecsFor.Helpers.Web.Mvc;
+
+namespace Deck36.UnitSpecs.Web.Helpers
+{
+    public class BootstrapHelperSpecs
+    {
+        public class when_creating_a_bootstrap_label : SpecsFor<FakeHtmlHelper>
+        {
+            private IHtmlString _label;
+
+            protected override void When()
+            {
+                _label = SUT.BootstrapLabel("FirstName");
+            }
+
+            [Test]
+            public void then_it_creates_label()
+            {
+                _label.ToHtmlString();
+            }
+        }
+    }
+}
---
+++ Deck36.Web/Models/Officer.cs
@@ -0,0 +1,13 @@
+﻿namespace Deck36.Web.Models
+{
+    public class Officer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Rank { get; set; }
+    }
+}
